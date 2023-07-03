e1ec301e6685bbb1841f322b6d78c13f1d9dacc0
Chaima Gharbi <Chaima.GH6602@gmail.com>
2023-07-03 18:52:45 +0100

    changed calendar to french

--- client-side-main/src/pages/availability/components/index.tsx
+++ client-side-main/src/pages/availability/components/index.tsx
@@ -1,10 +1,40 @@
-import { Calendar, momentLocalizer, SlotInfo } from 'react-big-calendar'
-import moment from 'moment'
+import { Calendar, dateFnsLocalizer, SlotInfo } from 'react-big-calendar'
 import 'react-big-calendar/lib/css/react-big-calendar.css'
 import { useCallback } from 'react'
 import { useGetUser } from 'app/store/hooks'
+import format from 'date-fns/format';
+import parse from 'date-fns/parse';
+import startOfWeek from 'date-fns/startOfWeek';
+import getDay from 'date-fns/getDay';
+import fr from 'date-fns/locale/fr';
 
-const localizer = momentLocalizer(moment)
+const messages = {
+  allDay: 'Tous les jours',
+  previous: 'Précédent',
+  next: 'Suivant',
+  today: "Aujourd'hui",
+  month: 'Mois',
+  week: 'Semaine',
+  day: 'Jour',
+  agenda: 'Agenda',
+  date: 'Date',
+  time: 'Heure',
+  event: 'Événement',
+};
+
+const locales = {
+  fr: fr,
+};
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+});
+
+
 
 export default function AvailabilityCalendar({ events, onSlotSelect }) {
   const onSelectSlot = useCallback((selected: SlotInfo) => {
@@ -25,6 +55,7 @@ export default function AvailabilityCalendar({ events, onSlotSelect }) {
   return (
     <Calendar
       localizer={localizer}
+      culture='fr'
       events={events}
       views={{
         week: true,
@@ -34,6 +65,7 @@ export default function AvailabilityCalendar({ events, onSlotSelect }) {
       startAccessor="start"
       endAccessor="end"
       style={{ height: 500 }}
+      messages={messages}
       defaultView="week"
       min={new Date(2017, 10, 0, 6, 0, 0)}
       max={new Date(2017, 10, 0, 20, 0, 0)}
@@ -43,6 +75,16 @@ export default function AvailabilityCalendar({ events, onSlotSelect }) {
 }
 
 const today = new Date()
+
+// const events = [
+//   {
+//     allDay: false,
+//     title: 'Entretien de John Doe',
+//     start: today,
+//     end: new Date(Number(today) + 3600000),
+//   },
+// ]
+
 
 // const events = [
 //   {
