dca3fbb0697ade8d62932744940de1a1f20266aa
Dhouib Mohamed <mohamed.dhouib@insat.ucar.tn>
2023-07-07 18:18:14 +0100

    general architecture optimizing

--- back-side-main/src/shared/utils.ts
+++ back-side-main/src/shared/utils.ts
@@ -1,3 +1,23 @@
+import * as axios from "axios";
+
+const getPositionFromAddress = async (address: any) => {
+
+    try {
+        const reponse = await axios.default.get(
+            `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.GOOGLE_MAP_KEY}`
+        );
+
+        if (reponse.data.results.length === 0) {
+            return undefined;
+        } else {
+            return reponse.data.results[0].geometry.location;
+        }
+    } catch (error) {
+
+    }
+}
+
+
 function equalArrays(first, second) {
     if (first.length !== second.length) return false;
     for (let i = 0; i < first.length; i++) {
@@ -19,4 +39,5 @@ function pick(v, old) {
     return old;
 }
 
-export {equalArrays, pick}
+
+export {getPositionFromAddress, equalArrays, pick}
