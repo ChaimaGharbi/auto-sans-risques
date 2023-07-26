fb81c93674a076c840bd8d776a322539198545a7
Dhouib Mohamed <mohamed.dhouib@insat.ucar.tn>
2023-07-26 16:38:02 +0100

    using reverse proxy in the gateway

---
+++ backend/_gateway/src/app.middleware.ts
@@ -0,0 +1,40 @@
+import {Injectable, NestMiddleware} from '@nestjs/common';
+import {Request, Response} from 'express';
+import {createProxyMiddleware} from 'http-proxy-middleware';
+import {options, routes} from "./config";
+
+
+@Injectable()
+export class ReverseProxyMiddleware implements NestMiddleware {
+    use(req: Request, res: Response, next: () => void) {
+        try {
+            const config = this.getConfig(req.url)
+            const target = `http://${config.host}:${config.port}`
+
+            const proxy = createProxyMiddleware({
+                target,
+                changeOrigin: true,
+            });
+            proxy(req, res, next);
+        } catch (e) {
+            console.log(e)
+            res.status(500).send({message: "Internal server error"})
+        }
+    }
+
+    private getConfig(url) {
+        let config = options["admin"]
+
+        const route = url.split('/')[1];
+
+        for (const key in routes) {
+            if (routes[key].includes(route)) {
+                config = options[key];
+                break;
+            }
+        }
+        console.log(url)
+        console.log(config)
+        return config
+    }
+}
---
+++ backend/_gateway/src/config.ts
@@ -0,0 +1,46 @@
+const env = "dev"
+const devOptions = {
+    admin: {
+        host: 'localhost',
+        port: 8001,
+    },
+    communication: {
+        host: 'localhost',
+        port: 8002,
+    },
+    diagnosis: {
+        host: 'localhost',
+        port: 8003,
+    },
+    user_management: {
+        host: 'localhost',
+        port: 8004,
+    }
+}
+
+const prodOptions = {
+    admin: {
+        host: 'admin',
+        port: 8001,
+    },
+    communication: {
+        host: 'communication',
+        port: 8002,
+    },
+    diagnosis: {
+        host: 'diagnosis',
+        port: 8003,
+    },
+    user_management: {
+        host: 'user_management',
+        port: 8004,
+    }
+}
+export const options = env === "dev" ? devOptions : prodOptions
+
+export const routes = {
+    admin: ["admin"],
+    communication: ["pack", 'contact', 'assistance', 'article', 'ads'],
+    diagnosis: ['reservation', 'reclamation', 'rapport', 'avis'],
+    user_management: ['payment', 'expert', 'client', 'auth', 'disponibilte']
+}
