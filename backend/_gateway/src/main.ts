cd2f5fbb3ed77b08c010ded7986f723e5fe01d08
Dhouib Mohamed <mohamed.dhouib@insat.ucar.tn>
2023-07-28 10:08:21 +0100

    corricting gateway error

--- backend/_gateway/src/main.ts
+++ backend/_gateway/src/main.ts
@@ -1,8 +1,6 @@
 import {NestFactory} from '@nestjs/core';
 import {AppModule} from './app.module';
 
-const helmet = require('helmet');
-
 async function bootstrap() {
     const app = await NestFactory.create(AppModule, {
         logger: console,
